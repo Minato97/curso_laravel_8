68b27ea005a34409ace922fd498ae5a939e90711
Minato 97 <ignacio.andrade8033@alumnos.udg.mx>
2022-02-16 15:00:59 -0600

    Formularios y registros

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.plantilla')
 
-@section('title','curso' . $curso)
+@section('title','curso' . $curso->name)
 
 @section('content')
-<h1>Bienvenido al curso <?php echo $curso;?>  </h1>
+<h1>Bienvenido al curso <?php echo $curso->name;?>  </h1>
+<a href="{{route('cursos.index')}}">Volver a curso</a>
+<br>
+<a href="{{route('cursos.edit',$curso)}}">Editar curso</a>
+    <p><strong>Categoria:</strong>{{$curso->categoria}}</p>
+    <p><strong>Descripcion:</strong>{{$curso->descripcion}}</p>
 @endsection
 
 
